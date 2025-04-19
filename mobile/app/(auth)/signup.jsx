a5fbecd371af6e444830a7b7dc86bff09a0a9229
Shaheen Naiyer <snshohin@gmail.com>
2025-04-19 13:12:40 +0530

    mobile app first commit

---
+++ mobile/app/(auth)/signup.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function signup() {
+  return (
+    <View>
+      <Text>Signup</Text>
+    </View>
+  )
+}
---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return 
+  <Stack screenOptions={{headerShown:true}}>;
+    <Stack.Screen name="index" options={{title:"Home"}}/>
+  </Stack>
+}
