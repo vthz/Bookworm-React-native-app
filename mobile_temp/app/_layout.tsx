aaec87929c7395585c6bf2c6cadceae39009e152
Shaheen Naiyer <snshohin@gmail.com>
2025-04-21 23:30:32 +0530

    removed unwanted folder

--- mobile_temp/app/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function RootLayout() {
-  // return <Stack />;
-  return (
-  <Stack screenOptions={{headerShown:true}}>;
-    <Stack.Screen name="index" options={{title:"Home"}}/>
-  </Stack> )
-}
